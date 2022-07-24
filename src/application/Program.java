a3faefdbe6e7a5e26d7b550e56709a7a0fc49c13
Caio de Assis Guimaraes <caiodeassisguimaraes@hotmail.com>
2022-07-24 08:28:40 -0300

    logica de combate ajustada e completa na Classe Program, próximo passo: vriar método takeDamage na Classe Champion

--- src/application/Program.java
+++ src/application/Program.java
@@ -45,44 +45,43 @@ public class Program {
 
 		for (int i = 0; i < turnos; i++) {
 			System.out.println("Resultado do turno " + (i + 1) + ":");
-			if (champion1.vida == 0 || champion2.vida == 0) {
-				
+
+			int combate1 = (champion1.vida + champion1.armadura) - champion2.ataque;
+			if (combate1 < 0) {
+				combate1 = 0;
+			}
+			if (champion1.armadura > champion2.ataque) {
+				champion1.vida -= 1;
+				combate1 = champion1.vida;
+			}
+			champion1.vida = combate1;
+			int combate2 = (champion2.vida + champion2.armadura) - champion1.ataque;
+			if (combate2 < 0) {
+				combate2 = 0;
+			}
+			if (champion2.armadura > champion1.ataque) {
+				champion2.vida -= 1;
+				combate2 = champion2.vida;
+			}
+			champion2.vida = combate2;
+			if (champion1.vida == 0) {
 				turnos = i;
-				System.out.println(champion1.nome + ": " + champion1.vida + " de vida");
+				System.out.println(champion1.nome + ": " + champion1.vida + " de vida (morreu)");
 				System.out.println(champion2.nome + ": " + champion2.vida + " de vida");
 				System.out.println();
-				System.out.println("MORREU");
-				
+			} else if (champion2.vida == 0) {
+				turnos = i;
+				System.out.println(champion1.nome + ": " + champion1.vida + " de vida");
+				System.out.println(champion2.nome + ": " + champion2.vida + " de vida (morreu)");
+				System.out.println();
 			} else {
-				int combate1 = (champion1.vida + champion1.armadura) - champion2.ataque;
-				if (combate1 < 0) {
-					combate1 = 0;
-				}
-				if (champion1.armadura > champion2.ataque) {
-					champion1.vida -= 1;
-					combate1 = champion1.vida;
-				}
-				champion1.vida = combate1;
-				int combate2 = (champion2.vida + champion2.armadura) - champion1.ataque;
-				if (combate2 < 0) {
-					combate2 = 0;
-				}
-				if (champion2.armadura > champion1.ataque) {
-					champion2.vida -= 1;
-					combate2 = champion2.vida;
-				}
-				champion2.vida = combate2;
 				System.out.println(champion1.nome + ": " + champion1.vida + " de vida");
 				System.out.println(champion2.nome + ": " + champion2.vida + " de vida");
 				System.out.println();
 			}
-
 		}
-
 		System.out.print("FIM DO COMBATE");
-
 		sc.close();
-
 	}
 
 }
