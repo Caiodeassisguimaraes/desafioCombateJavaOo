9fbaef327e95f3ccbc5cfdfb52145a38fb0df95a
Caio de Assis Guimaraes <caiodeassisguimaraes@hotmail.com>
2022-07-24 21:52:30 -0300

    desafio quase finalizado, falta apenas concertar a lógica do método takeDamage(Champion other) pois no Exemplo 2 o cálculo está dando errado

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,8 +2,7 @@ package application;
 
 import java.util.Locale;
 import java.util.Scanner;
-
-import util.Champion;
+import entities.Champion;
 
 public class Program {
 
@@ -13,75 +12,48 @@ public class Program {
 
 		System.out.println("Digite os dados do primeiro campeão: ");
 		System.out.print("Nome: ");
-		String nome = sc.nextLine();
+		String name = sc.nextLine();
 		System.out.print("Vida inicial: ");
-		int vida = sc.nextInt();
+		int life = sc.nextInt();
 		System.out.print("Ataque: ");
-		int ataque = sc.nextInt();
+		int attack = sc.nextInt();
 		System.out.print("Armadura: ");
-		int armadura = sc.nextInt();
+		int armor = sc.nextInt();
 
-		Champion champion1 = new Champion(nome, vida, ataque, armadura);
+		Champion champion1 = new Champion(name, life, attack, armor);
 
 		System.out.println();
 
 		System.out.println("Digite os dados do segundo campeão: ");
 		System.out.print("Nome: ");
 		sc.nextLine();
-		nome = sc.nextLine();
+		name = sc.nextLine();
 		System.out.print("Vida inicial: ");
-		vida = sc.nextInt();
+		life = sc.nextInt();
 		System.out.print("Ataque: ");
-		ataque = sc.nextInt();
+		attack = sc.nextInt();
 		System.out.print("Armadura: ");
-		armadura = sc.nextInt();
+		armor = sc.nextInt();
 
-		Champion champion2 = new Champion(nome, vida, ataque, armadura);
+		Champion champion2 = new Champion(name, life, attack, armor);
 
 		System.out.println();
 
 		System.out.print("Quantos turnos você deseja executar? ");
-		int turnos = sc.nextInt();
+		int rounds = sc.nextInt();
 
 		System.out.println();
 
-		for (int i = 0; i < turnos; i++) {
+		for (int i = 0; i < rounds; i++) {
 			System.out.println("Resultado do turno " + (i + 1) + ":");
-
-			int combate1 = (champion1.getVida() + champion1.getArmadura()) - champion2.getAtaque();
-			if (combate1 < 0) {
-				combate1 = 0;
+			champion1.takeDamage(champion2);
+			champion2.takeDamage(champion1);
+			System.out.println(champion1.status());
+			System.out.println(champion2.status());
+			if (champion1.getLife() == 0 || champion2.getLife() == 0) {
+				rounds = i;
 			}
-			if (champion1.getArmadura() > champion2.getAtaque()) {
-				champion1.setVida(champion1.getVida() - 1);
-				combate1 = champion1.getVida();
-			}
-			champion1.setVida(combate1);
-			
-			int combate2 = (champion2.getVida() + champion2.getArmadura()) - champion1.getAtaque();
-			if (combate2 < 0) {
-				combate2 = 0;
-			}
-			if (champion2.getArmadura() > champion1.getAtaque()) {
-				champion2.setVida(champion2.getVida() - 1);
-				combate2 = champion2.getVida();
-			}
-			champion2.setVida(combate2);
-			if (champion1.getVida() == 0) {
-				turnos = i;
-				System.out.println(champion1.status());
-				System.out.println(champion2.getNome() + ": " + champion2.getVida() + " de vida");
-				System.out.println();
-			} else if (champion2.getVida() == 0) {
-				turnos = i;
-				System.out.println(champion1.getNome() + ": " + champion1.getVida() + " de vida");
-				System.out.println(champion2.status());
-				System.out.println();
-			} else {
-				System.out.println(champion1.getNome() + ": " + champion1.getVida() + " de vida");
-				System.out.println(champion2.getNome() + ": " + champion2.getVida() + " de vida");
-				System.out.println();
-			}
+			System.out.println();
 		}
 		System.out.print("FIM DO COMBATE");
 		sc.close();
---
+++ src/entities/Champion.java
@@ -0,0 +1,77 @@
+package entities;
+
+public class Champion {
+	// atributos da Classe
+	private String name;
+	private int life;
+	private int attack;
+	private int armor;
+
+	// Construtor
+
+	public Champion(String nome, int vida, int ataque, int armadura) {
+		this.name = nome;
+		this.life = vida;
+		this.attack = ataque;
+		this.armor = armadura;
+	}
+
+	// Metodos Get e Set
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getLife() {
+		return life;
+	}
+
+	public void setLife(int vida) {
+		this.life = vida;
+	}
+
+	public int getAttack() {
+		return attack;
+	}
+
+	public void setAttack(int ataque) {
+		this.attack = ataque;
+	}
+
+	public int getArmor() {
+		return armor;
+	}
+
+	public void setArmor(int armadura) {
+		this.armor = armadura;
+	}
+
+	// Metodos
+
+	public void takeDamage(Champion other) {
+		//Corrigir essa logica
+		life = (life + armor) - other.getAttack();
+		if (life < 0) {
+			life = 0;
+		} else if (armor > other.getAttack()) {
+			life -= 1;
+		}
+
+	}
+
+	public String status() {
+
+		if (life == 0) {
+			return name + ": " + life + " de vida (morreu)";
+
+		} else {
+			return name + ": " + life + " de vida";
+		}
+
+	}
+
+}
